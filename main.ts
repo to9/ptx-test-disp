27c4bccd75159a3fb0af2a3d33b3c5628662235f
G.D <gaodongzi@126.com>
2024-12-27 10:24:16 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,6 @@ loops.everyInterval(1000, function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(1000)
     basic.showIcon(IconNames.No)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
 })
